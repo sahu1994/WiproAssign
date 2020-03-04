9f475a31a6275dc37dba7a14b27ddb7c13f89006
sahu.girish78@gmail.com <22S@hu11>
2020-03-04 13:19:07 +0530

    Retrofit methods removed from application

--- app/src/main/java/com/wipro/wiproassignment/utils/Application.kt
+++ app/src/main/java/com/wipro/wiproassignment/utils/Application.kt
@@ -23,26 +23,4 @@ class ListApp : Application() {
     }
 
 
-    fun provideRetrofitInterface(): RestAPI {
-        synchronized(this) {
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(getClient())
-                .build()
-            return retrofit.create(RestAPI::class.java)
-        }
-    }
-
-    private fun getClient(): OkHttpClient {
-        val okHttpClient: OkHttpClient.Builder = OkHttpClient.Builder()
-        okHttpClient.connectTimeout(1, TimeUnit.MINUTES)
-            .readTimeout(1, TimeUnit.MINUTES)
-            .writeTimeout(1, TimeUnit.MINUTES)
-            .retryOnConnectionFailure(false)
-
-        return okHttpClient.build()
-    }
-
-
 }
